522cf668f55a927949d1ed7e89830fab45e0f0be
Admin <adminsf@Admins-MacBook-Pro.local>
2022-03-20 11:38:06 -0700

    add profile

---
+++ react-version/src/components/TweetForm.js
@@ -0,0 +1,17 @@
+function TweetForm() {
+  const placeholderVal = "What are you humming about?";
+
+  return (
+    <section class="newtweet">
+      <form onClick={(event) => {
+        event.preventDefault();
+      }} method="post" action="/tweets" class="newtweet__form">
+        <textarea class="form__textarea" name="text" placeholder={placeholderVal}></textarea>
+        <input type="submit" value="Tweet" class="form__input" />
+        <span class="form__counter">140</span>
+      </form>
+    </section>
+  );
+}
+
+export default TweetForm;
